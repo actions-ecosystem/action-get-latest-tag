51b604461a49e627ecc94ec96edf960130cf8d16
micnncim <micnncim@gmail.com>
2020-09-18 18:05:30 +0900

    Suppress 'git fetch --unshallow' error

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 set -e
 
 git fetch --tags
-git fetch --prune --unshallow
+# This suppress an error occurred when the repository is a complete one.
+git fetch --prune --unshallow || true
 
 latest_tag=''
 
@@ -13,7 +14,7 @@ if [ "${INPUT_SEMVER_ONLY}" = 'false' ]; then
 else
   # Get a latest tag in the shape of semver.
   for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
-    tag="${ref#refs/tags/}"
+    readonly tag="${ref#refs/tags/}"
     if echo "${tag}" | grep -Eq '^v?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$'; then
       latest_tag="${tag}"
       break
