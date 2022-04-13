b7c32daec3395a9616f88548363a42652b22d435
Motoki Wu <tokestermw@gmail.com>
2022-04-13 06:22:32 -0700

    Fix `fatal: unsafe repository ('/github/workspace' is owned by someon… (#19)
    * Fix `fatal: unsafe repository ('/github/workspace' is owned by someone else)`
    
    Looks like there were some changes to git, and this action currently errors out.
    
    ```
    fatal: unsafe repository ('/github/workspace' is owned by someone else)
    ```
    
    The fix is to add a safe directory (tested in a separate repo).
    
    Related Stack Overflow: https://stackoverflow.com/questions/71849415/cannot-add-parent-directory-to-safe-directory-on-git
    
    * Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git config --global --add safe.directory /github/workspace
+
 git fetch --tags
 # This suppress an error occurred when the repository is a complete one.
 git fetch --prune --unshallow || true
